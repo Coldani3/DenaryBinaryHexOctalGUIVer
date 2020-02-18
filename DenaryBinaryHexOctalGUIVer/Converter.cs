59059628bf6c160db5c8fa8d8438d705a7cb11c6
Coldani3 <p110117069@gmail.com>
2020-02-18 12:57:36 +0000

    Initial Commit

---
+++ DenaryBinaryHexOctalGUIVer/Converter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenaryBinaryHexOctalGUIVer
+{
+    public class Converter
+    {
+        static Dictionary<int, char> HexConversions = new Dictionary<int, char>
+        {
+            { 10, 'A' },
+            { 11, 'B' },
+            { 12, 'C' },
+            { 13, 'D' },
+            { 14, 'E' },
+            { 15, 'F' }
+        };
+
+        public static string ConvertToBase(string input, Base from, Base to)
+        {
+            int raw = 0;
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                raw += (Convert.ToInt32(input[input.Length - i - 1]) - 48) * (int)Math.Pow((int)from, i);
+            }
+
+            bool passedBeginningZeros = false;
+            for (int i = 32; i >= 0; i--)
+            {
+                int divided = (int)Math.Floor(raw / Math.Pow((int)to, i));
+
+                if (divided > 0) passedBeginningZeros = true;
+
+                if (divided > 9) output.Append(HexConversions[divided]);
+                else if (passedBeginningZeros) output.Append(divided);
+
+                raw -= (int)Math.Pow((int)to, i) * divided;
+            }
+
+            return output.ToString();
+        }
+
+        public static bool ValidateForBase(string input, Base numBase)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                char letter = input.ToUpper()[i];
+                if (Char.IsLetterOrDigit(letter))
+                {
+                    if (Char.IsDigit(letter))
+                    {
+                        if (Convert.ToInt32(letter) - 49 > ((int) numBase) - 1)
+                        {
+                            return false;
+                        }
+                    }
+                    else if (Char.IsLetter(letter) && Convert.ToInt32(letter) - 49 > 'F')
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public enum Base
+    {
+        BINARY = 2,
+        OCTAL = 8,
+        DENARY = 10,
+        HEXADECIMAL = 16
+    }
+}
---
+++ DenaryBinaryHexOctalGUIVer/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DenaryBinaryHexOctalGUIVer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Dictionary<string, Base> ScreenNameToBase = new Dictionary<string, Base>
+        {
+            { "Binary (Base 2)", Base.BINARY },
+            { "Octal (Base 8)", Base.OCTAL },
+            { "Denary (Base 10)", Base.DENARY },
+            { "Hexadecimal (Base 16)", Base.HEXADECIMAL }
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //BitmapImage image = new BitmapImage(new Uri(@"icon.png", UriKind.RelativeOrAbsolute));
+            //this.Icon = image;
+
+            foreach (string numBase in ScreenNameToBase.Keys)
+            {
+                FromBox.Items.Add(numBase);
+                ToBox.Items.Add(numBase);
+            }
+
+            this.FromBox.SelectedIndex = 2;
+            this.ToBox.SelectedIndex = 0;
+        }
+
+        private void InputBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            CheckInputAndUpdate();
+        }
+
+        private void CheckInputAndUpdate()
+        {
+            if (FromBox.SelectedIndex != -1)
+            {
+                string currInput = InputBox.Text;
+
+                if (!ValidateInput(InputBox.Text, ScreenNameToBase[(string)FromBox.SelectedItem])) InputBox.Text = "";
+                else UpdateOutput();
+            }
+        }
+
+        private void BaseBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FromBox.SelectedIndex != -1)
+            {
+                if (!Converter.ValidateForBase(InputBox.Text.ToUpper(), ScreenNameToBase[(string)FromBox.SelectedItem])) InputBox.Text = "";
+                UpdateOutput();
+            }
+        }
+
+        public bool ValidateInput(string input, Base fromBase)
+        {
+            if (!Converter.ValidateForBase(InputBox.Text.ToUpper(), fromBase))
+            {
+                MessageBox.Show("Invalid input! It must match the specified base!");
+                return false;
+            }
+
+            return true;
+        }
+
+        public void UpdateOutput()
+        {
+            if (FromBox.SelectedIndex != -1 && ToBox.SelectedIndex != -1 && InputBox.Text != "")
+            {
+                Base fromBase = ScreenNameToBase[(string)FromBox.SelectedItem];
+                Base toBase = ScreenNameToBase[(string)ToBox.SelectedItem];
+
+                OutputBox.Text = Converter.ConvertToBase(InputBox.Text, fromBase, toBase);
+            }
+            else OutputBox.Text = "";
+        }
+
+        private void Window_UnfocusOnClick(object sender, MouseButtonEventArgs e)
+        {
+            ((UIElement) sender).Focus();
+        }
+
+        private void ForceCalcButton_Click(object sender, RoutedEventArgs e)
+        {
+            CheckInputAndUpdate();
+        }
+
+        private void InputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                CheckInputAndUpdate();
+            }
+        }
+
+        private void SwapBasesButton_Click(object sender, RoutedEventArgs e)
+        {
+            object fromBase = FromBox.SelectedItem;
+            FromBox.SelectedItem = ToBox.SelectedItem;
+            ToBox.SelectedItem = fromBase;
+            CheckInputAndUpdate();
+        }
+    }
+}
